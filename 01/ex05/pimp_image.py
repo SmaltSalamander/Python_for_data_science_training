847852b7a527f9084902a41c55181bca5dce42d5
sbienias <sandrahanrin@gmail.com>
2023-05-14 18:40:54 +0200

    refactor ft_red and blue

--- 01/ex05/pimp_image.py
+++ 01/ex05/pimp_image.py
@@ -1,6 +1,5 @@
 # %%
 from load_image import ft_load
-from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -10,8 +9,7 @@ def ft_invert(array) -> np.array:
 
 def ft_red(array) -> np.array:
     red_image = np.array(array)
-    red_image[...,1] = 0
-    red_image[...,2] = 0
+    red_image[...,1:] = 0
     return red_image
 
 def ft_green(array) -> np.array:
@@ -22,21 +20,20 @@ def ft_green(array) -> np.array:
 
 def ft_blue(array) -> np.array:
     blue_image = np.array(array)
-    blue_image[...,0] = 0
-    blue_image[...,1] = 0
+    blue_image[...,:2] = 0
     return blue_image
 
 def ft_grey(array) -> np.array:
     max_rgb = np.amax(array)
-    gray_img = np.array(np.max(array, axis=2) / max_rgb)
-    return gray_img
+    grey_img = np.array(np.max(array, axis=2) / max_rgb)
+    return grey_img
 
 
 def main():
     landscape = ft_load("landscape.jpg")
     ft_grey(landscape)
-    plt.imshow(ft_grey(landscape), cmap='gray')
-    # plt.imshow(ft_invert(landscape))
+    # plt.imshow(ft_grey(landscape), cmap='gray')
+    plt.imshow(ft_blue(landscape))
 
 if __name__ == "__main__":
     main()
